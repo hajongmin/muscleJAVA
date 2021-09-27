821289c10e51543dbecd3cffdc29f3ff510f0c4e
hajongmin <hjm7130@naver.com>
2021-09-27 15:10:22 +0900

    tset22

--- src/muscleJAVA/jongmin.java
+++ src/muscleJAVA/jongmin.java
@@ -3,8 +3,10 @@ package muscleJAVA;
 public class jongmin {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 		System.out.print("jongmin test3");
-	
+		System.out.print("taeuk test");
+		System.out.print("test11");
 	}
 }
